a96cb5cddd1876f9aa960a90fb112c6cedad9464
masaTora <omadedou@gmail.com>
2017-04-03 03:49:27 +0800

    make reducer to pure function

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,36 +2,65 @@ const reducer = (state = [], action) => {
     const i = action.i;
     switch (action.type) {
         case 'ON_HEART_CLICK':
-            state[i].likes.like = !state[i].likes.like;
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  likes: {like: !state[i].likes.like}},
+                ...state.slice(i + 1)
+            ];
         case 'ON_AVATAR_ENTER':
-            state[i]['avatar'] = 'icon-emo-laugh';
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  avatar: 'icon-emo-laugh'},
+                ...state.slice(i + 1)
+            ];
         case 'ON_AVATAR_LEAVE':
-            state[i]['avatar'] = 'icon-emo-happy';
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  avatar: 'icon-emo-happy'},
+                ...state.slice(i + 1)
+            ];
         case 'ON_OK_ENTER':
-            state[i]['icon_ok'] = 'icon-ok-circled2';
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  icon_ok: 'icon-ok-circled2'},
+                ...state.slice(i + 1)
+            ];
         case 'ON_OK_LEAVE':
-            state[i]['icon_ok'] = 'icon-ok-circled';
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  icon_ok: 'icon-ok-circled'},
+                ...state.slice(i + 1)
+            ];
         case 'ON_OK_CLICK':
-            state[i].comments.unshift(action.comment);
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  comments: [action.comment].concat(state[i].comments)},
+                ...state.slice(i + 1)
+            ];
         case 'ON_TEXT_CLICK':
-            state[i].isText = false;
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  isText: false},
+                ...state.slice(i + 1)
+            ];
         case 'ON_COMMENT_ENTER':
-            state[i]['icon_comment'] = 'icon-comment-empty';
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  icon_comment: 'icon-comment-empty'},
+                ...state.slice(i + 1)
+            ];
         case 'ON_COMMENT_LEAVE':
-            state[i]['icon_comment'] = 'icon-comment';
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  icon_comment: 'icon-comment'},
+                ...state.slice(i + 1)
+            ];
         case 'ON_ARTICLE_SEND':
-            state[i].isText = true;
-            state[i].article = action.article;
-            return [...state];
+            return [
+                ...state.slice(0, i),
+                {...state[i],  isText: true, article: action.article},
+                ...state.slice(i + 1)
+            ];
         default:
             return state;
     }
